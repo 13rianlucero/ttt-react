8b1befdafd1208ee4f2644008d27389dc9e3ede9
Brian Lucero <13rianlucero@csu.fullerton.edu>
2022-09-16 22:48:15 -0700

    commit from macbook

---
+++ public/like_button.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You HATE this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
